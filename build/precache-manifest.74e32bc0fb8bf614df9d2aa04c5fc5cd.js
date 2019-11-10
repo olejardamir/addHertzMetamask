64ee342e257ee7addd879b9f56f1fc9e71f9c6e3
olejardamir <olejar.damir@gmail.com>
2019-11-10 10:08:58 -0500

    mssg

---
+++ build/precache-manifest.74e32bc0fb8bf614df9d2aa04c5fc5cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22bebf2abb300cb94c2d4a190b2e2f63",
+    "url": "/Add-Token/index.html"
+  },
+  {
+    "revision": "9cd4951ccbc2a409ad97",
+    "url": "/Add-Token/static/css/main.32d1f9de.chunk.css"
+  },
+  {
+    "revision": "d3be8d827ec9583d956e",
+    "url": "/Add-Token/static/js/2.428bf56b.chunk.js"
+  },
+  {
+    "revision": "9cd4951ccbc2a409ad97",
+    "url": "/Add-Token/static/js/main.a8592360.chunk.js"
+  },
+  {
+    "revision": "3523ba9fddafeaae299d",
+    "url": "/Add-Token/static/js/runtime-main.562ca8ff.js"
+  },
+  {
+    "revision": "622da729656ea7334051151cbb23e989",
+    "url": "/Add-Token/static/media/coin.622da729.jpg"
+  },
+  {
+    "revision": "14c35a626d4aa99499664ab05c04ac52",
+    "url": "/Add-Token/static/media/download-metamask.14c35a62.png"
+  },
+  {
+    "revision": "508f95cd9cf5232c07f9ec5c650664c8",
+    "url": "/Add-Token/static/media/switch-network.508f95cd.jpg"
+  }
+]);
